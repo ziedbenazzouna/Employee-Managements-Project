dd55493f63c6db98c102824a64268db24497076e
Zied Ben Azzouna <zied.benazzouna@gmail.com>
2022-11-20 07:53:05 -0400

    Set Angular Default Form Validation with Custom Password Comparison

--- EmployeeManagementsProject.Web/ClientApp/src/app/shared/user.service.ts
+++ EmployeeManagementsProject.Web/ClientApp/src/app/shared/user.service.ts
@@ -10,12 +10,24 @@ export class UserService {
   constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   formModel = this.fb.group({
-    UserName: [''],
-    Email: [''],
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
     FullName: [''],
     Passwords: this.fb.group({
-      Password: [''],
-      ConfirmPassword: ['']   
-    })
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]   
+    },{ validator: this.comparePasswords })
   });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //passwordMismatch 
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+      if (fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else
+        confirmPswrdCtrl.setErrors(null);
+    }
+  }
 }
