c4d052460c47f57b84013c1f9cbd17fd8701e03f
Zied Ben Azzouna <zied.benazzouna@gmail.com>
2022-12-08 08:22:03 -0400

    Remove unnecessary import

--- EmployeeManagementsProject.Web/ClientApp/src/app/employees/employee-list/employee-list.component.ts
+++ EmployeeManagementsProject.Web/ClientApp/src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
-import { Employee } from 'src/app/shared/employee.model';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
